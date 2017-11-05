e9bd240ed8f6fc0d2679a392e5c4e8e7fab048ae
Romain KOENIG <romain.koe@gmail.com>
2017-11-05 22:44:05 +0100

    first commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+Express - Auth0 - Vue2
+=============
+
+Service de base pour faire une auth avec auth0.
+
+Permet le login de l'utilisateur.
+
+A une vue de base.
+
+A une API avec auth.
---
+++ client/src/utils/auth.js
@@ -0,0 +1,103 @@
+import decode from 'jwt-decode';
+// import axios from 'axios';
+import auth0 from 'auth0-js';
+import Router from 'vue-router';
+// import Auth0Lock from 'auth0-lock';
+
+const ID_TOKEN_KEY = 'id_token';
+const ACCESS_TOKEN_KEY = 'access_token';
+
+const CLIENT_ID = 'mxO9aRdF0XH1Norbq2n7k4XTzq5VGiFy';
+const CLIENT_DOMAIN = 'koenigr.eu.auth0.com';
+const REDIRECT = 'http://localhost:8080/callback';
+const SCOPE = 'full_access';
+const AUDIENCE = 'https://test.com';
+
+const auth = new auth0.WebAuth({
+  clientID: CLIENT_ID,
+  domain: CLIENT_DOMAIN,
+});
+
+export function login() {
+  auth.authorize({
+    responseType: 'token id_token',
+    redirectUri: REDIRECT,
+    audience: AUDIENCE,
+    scope: SCOPE,
+  });
+}
+
+const router = new Router({
+  mode: 'history',
+});
+
+export function logout() {
+  clearIdToken();
+  clearAccessToken();
+  router.go('/');
+}
+
+export function requireAuth(to, from, next) {
+  if (!isLoggedIn()) {
+    next({
+      path: '/',
+      query: { redirect: to.fullPath },
+    });
+  } else {
+    next();
+  }
+}
+
+export function getIdToken() {
+  return localStorage.getItem(ID_TOKEN_KEY);
+}
+
+export function getAccessToken() {
+  return localStorage.getItem(ACCESS_TOKEN_KEY);
+}
+
+function clearIdToken() {
+  localStorage.removeItem(ID_TOKEN_KEY);
+}
+
+function clearAccessToken() {
+  localStorage.removeItem(ACCESS_TOKEN_KEY);
+}
+
+// Helper function that will allow us to extract the access_token and id_token
+function getParameterByName(name) {
+  const match = RegExp(`[#&]${name}=([^&]*)`).exec(window.location.hash);
+  return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
+
+// Get and store access_token in local storage
+export function setAccessToken() {
+  const accessToken = getParameterByName('access_token');
+  localStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
+}
+
+// Get and store id_token in local storage
+export function setIdToken() {
+  const idToken = getParameterByName('id_token');
+  localStorage.setItem(ID_TOKEN_KEY, idToken);
+}
+
+export function isLoggedIn() {
+  const idToken = getIdToken();
+  return !!idToken && !isTokenExpired(idToken);
+}
+
+function getTokenExpirationDate(encodedToken) {
+  const token = decode(encodedToken);
+  if (!token.exp) { return null; }
+
+  const date = new Date(0);
+  date.setUTCSeconds(token.exp);
+
+  return date;
+}
+
+function isTokenExpired(token) {
+  const expirationDate = getTokenExpirationDate(token);
+  return expirationDate < new Date();
+}
